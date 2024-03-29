4b34988daa7a1ba5ebeaa8d41ed22d2afab74365
Barla Ankit <barla.x88@gmail.com>
2024-03-29 17:33:45 +0530

    Create design for mobile version

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,24 @@
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="container">
+      <div className="img-container"></div>
+      <div className="form-container">
+        <div>
+          <h1>Stay Updated!</h1>
+          <p>Join 60,000+ product managers receiving monthly updates on:</p>
+          <ul>
+            <li>Product discovery and building what matters</li>
+            <li>Measuring to ensure updates are a success</li>
+            <li>And much more!</li>
+          </ul>
+        </div>
+        <form>
+          <label htmlFor="email">Email address</label>
+          <input type="email" id="email" placeholder="email@company.com" />
+          <button>Subscribe to monthly newsletter</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 export default App;
