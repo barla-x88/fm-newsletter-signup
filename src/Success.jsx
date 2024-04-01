d4a71311dbe50e3a59c2291141a2c3e411875996
Barla Ankit <barla.x88@gmail.com>
2024-04-01 14:06:31 +0530

    Add success msg component

--- src/Success.jsx
+++ src/Success.jsx
@@ -1,4 +1,25 @@
-const Success = () => {
-  return <div>Success</div>;
+import successImg from './assets/images/icon-success.svg';
+
+const Success = ({ email }) => {
+  return (
+    <div className="success-container">
+      <div className="success-text">
+        <div>
+          <img src={successImg} alt="Sucess" />
+          <p className="success-heading">Thanks for subscribing!</p>
+        </div>
+        <div>
+          <p className="success-msg">
+            A confirmation email has been sent to
+            <span className="email"> {email}</span>. Please open it and click
+            the button inside to confirm your subscription.
+          </p>
+        </div>
+      </div>
+      <div className="success-btn">
+        <button>Dismiss message</button>
+      </div>
+    </div>
+  );
 };
 export default Success;
